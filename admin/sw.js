139aba08f1f8d0309f91a94d60982e65e85b15bb
Jefferson8149 <cadastrosjeh@gmail.com>
2026-09-15 17:18:09 -0300

    Atualiza loader e cache do PWA Admin

--- admin/sw.js
+++ admin/sw.js
@@ -1,6 +1,5 @@
-const CACHE='karaoke-maleta-admin-loader-v020';
+const CACHE='karaoke-maleta-admin-loader-v019';
 const ROOT='/karaoke-maleta-pwa/admin/';
-const ASSETS=[ROOT,ROOT+'index.html',ROOT+'manifest.webmanifest',ROOT+'icon.svg'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match(e.request)))});
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll([ROOT,ROOT+'index.html',ROOT+'manifest.webmanifest','/karaoke-maleta-pwa/singer/icon.svg'])).catch(()=>{}))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))),self.clients.claim()])));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
