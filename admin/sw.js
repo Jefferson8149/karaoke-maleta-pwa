5801106342165b5fc054941b12a25685a850b355
Jefferson8149 <cadastrosjeh@gmail.com>
2026-09-15 16:52:21 -0300

    Atualiza cache do PWA Admin

--- admin/sw.js
+++ admin/sw.js
@@ -1,1 +1,6 @@
-const C='km-admin-gh-v1',B='/karaoke-maleta-pwa/admin';self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll([B+'/',B+'/index.html',B+'/manifest.webmanifest',B+'/icon.svg'])))});self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin)e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
+const CACHE='karaoke-maleta-admin-loader-v020';
+const ROOT='/karaoke-maleta-pwa/admin/';
+const ASSETS=[ROOT,ROOT+'index.html',ROOT+'manifest.webmanifest',ROOT+'icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match(e.request)))});
