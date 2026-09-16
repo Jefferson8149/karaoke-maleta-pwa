d97ab172ee91f7a68f099c2c652f1bbc97a56461
Jefferson8149 <cadastrosjeh@gmail.com>
2026-09-16 00:40:14 -0300

    Atualiza cache para PNG real do logo

--- licencas/sw.js
+++ licencas/sw.js
@@ -1,6 +1,6 @@
-const CACHE='karaoke-maleta-licencas-v025';
+const CACHE='karaoke-maleta-licencas-v026';
 const ROOT='/karaoke-maleta-pwa/licencas/';
-const ASSETS=[ROOT,ROOT+'index.html',ROOT+'app.css?v=4',ROOT+'app.js?v=2',ROOT+'manifest.webmanifest?v=5',ROOT+'logo.svg?v=5'];
+const ASSETS=[ROOT,ROOT+'index.html',ROOT+'app.css?v=4',ROOT+'app.js?v=2',ROOT+'manifest.webmanifest?v=6',ROOT+'logo-km.png?v=6'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
