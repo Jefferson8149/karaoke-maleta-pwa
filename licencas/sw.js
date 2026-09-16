7679270e2f8d81e571aafeb76cfe7b0264f01557
Jefferson8149 <cadastrosjeh@gmail.com>
2026-09-16 00:29:42 -0300

    Atualiza cache para logo original do Karaoke Maleta

--- licencas/sw.js
+++ licencas/sw.js
@@ -1,6 +1,6 @@
-const CACHE='karaoke-maleta-licencas-v024';
+const CACHE='karaoke-maleta-licencas-v025';
 const ROOT='/karaoke-maleta-pwa/licencas/';
-const ASSETS=[ROOT,ROOT+'index.html',ROOT+'app.css?v=4',ROOT+'app.js?v=4',ROOT+'manifest.webmanifest?v=4',ROOT+'logo-km.svg?v=4'];
+const ASSETS=[ROOT,ROOT+'index.html',ROOT+'app.css?v=4',ROOT+'app.js?v=2',ROOT+'manifest.webmanifest?v=5',ROOT+'logo.svg?v=5'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
