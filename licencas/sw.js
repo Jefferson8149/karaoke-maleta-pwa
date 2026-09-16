7b11bbf21109a18945cdadd6f2ccf79d38fe09d1
Jefferson8149 <cadastrosjeh@gmail.com>
2026-09-16 00:09:53 -0300

    Atualiza cache para o logo local do PWA de licenças

--- licencas/sw.js
+++ licencas/sw.js
@@ -1,12 +1,12 @@
-const CACHE='karaoke-maleta-licencas-v021';
+const CACHE='karaoke-maleta-licencas-v022';
 const ROOT='/karaoke-maleta-pwa/licencas/';
-const ASSETS=[ROOT,ROOT+'index.html',ROOT+'app.css?v=3',ROOT+'app.js?v=3',ROOT+'manifest.webmanifest','/karaoke-maleta-pwa/singer/icon.svg'];
+const ASSETS=[ROOT,ROOT+'index.html',ROOT+'app.css?v=3',ROOT+'app.js?v=3',ROOT+'manifest.webmanifest',ROOT+'logo.svg?v=3'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
   const u=new URL(e.request.url);
-  if(u.pathname.includes('/licencas/') || u.pathname==='/karaoke-maleta-pwa/singer/icon.svg'){
+  if(u.pathname.includes('/licencas/')){
     e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)));
   }
 });
