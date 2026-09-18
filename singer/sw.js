190139655bb581ca542183cea978db7df89e68d3
Jefferson8149 <cadastrosjeh@gmail.com>
2026-09-18 10:04:04 -0300

    Atualiza service worker PWA Cantor v30

--- singer/sw.js
+++ singer/sw.js
@@ -1,5 +1,6 @@
-const CACHE='karaoke-maleta-singer-loader-v20';
+const CACHE='karaoke-maleta-singer-loader-v30';
 const ROOT='/karaoke-maleta-pwa/singer/';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll([ROOT,ROOT+'index.html',ROOT+'manifest.webmanifest','/karaoke-maleta-pwa/singer/icon.svg'])).catch(()=>{}))});
+const ASSETS=[ROOT,ROOT+'index.html',ROOT+'manifest.webmanifest?v=30',ROOT+'icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
