40253bbce7f0a8a01a2b6e04535ff2bd1dd45032
Jefferson8149 <cadastrosjeh@gmail.com>
2026-09-17 22:52:16 -0300

    Atualiza cache do PWA de licenças para o logo relativo

--- licencas/sw.js
+++ licencas/sw.js
@@ -1,6 +1,6 @@
-const CACHE='karaoke-maleta-licencas-v027';
+const CACHE='karaoke-maleta-licencas-v028';
 const ROOT='/karaoke-maleta-pwa/licencas/';
-const ASSETS=[ROOT,ROOT+'index.html',ROOT+'app.css?v=4',ROOT+'app.js?v=2',ROOT+'manifest.webmanifest?v=7'];
+const ASSETS=[ROOT,ROOT+'index.html',ROOT+'app.css?v=5',ROOT+'app.js?v=3',ROOT+'manifest.webmanifest?v=8',ROOT+'logo-km.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
